7e2630ecdd17860954d1f232b84097e898fe86a5
Fabricio Pereira Viana <125505755+Fabricio-P-Viana@users.noreply.github.com>
2024-02-23 10:57:12 -0300

    Update main.js removi o focus
    retirei o focus após clicar num botão pois estava abrindo a teclado no mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,6 @@ function Calculadora() {
     }};
   this.addNumDisplay = el => {
     this.display.value += el.innerText;
-    this.display.focus();
   };
   this.clear = () => this.display.value = '';
   this.del = () => this.display.value = this.display.value.slice(0, -1);
